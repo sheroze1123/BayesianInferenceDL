Faster model constrained adaptive sampling and correct gradient computation

applications/error_optimization.py
@@ -1,21 +1,21 @@
 from dolfin import *
 import numpy as np
-from forward_solve import forward, reduced_forward
+from forward_solve import Fin
 import scipy.optimize
 #  import time
 
 
-def gradient(z_vec, psi, phi, V):
+def gradient(z_vec, psi, phi, solver):
     '''
     Computes the gradient of the objective function G with respect to 
     the parameters z using the method of Lagrange multipliers
     '''
 
-    z = Function(V)
+    #TODO: Rewrite this in full PETSc for improved performance and standardization
+    z = Function(solver.V)
     z.vector().set_local(z_vec)
-    x_f, y, A, B, C, dA_dz = forward(z, V)
-    x = x_f.vector()[:]
-    A_r, B_r, C_r, x_r, y_r = reduced_forward(A, B, C, psi, phi)
+    x, y, A, B, C = solver.forward(z)
+    A_r, B_r, C_r, x_r, y_r = solver.reduced_forward(A, B, C, psi, phi)
     lambda_f = np.linalg.solve(A.T, C.T * (y_r - y))
 
     #TODO: Fix the singular matrix issues
@@ -25,28 +25,25 @@ def gradient(z_vec, psi, phi, V):
         print ("Singular matrix in reduced adjoint solve")
         import pdb; pdb.set_trace()
 
-    x_shape = x.shape
-    grad_G = np.zeros(x.shape)
-    x = x_f
-    z_hat = TrialFunction(V)
-    v = TestFunction(V)
+    z_hat = TrialFunction(solver.V)
+    v = TestFunction(solver.V)
 
     #TODO: Verify gradient
-    dR_dz = assemble(z_hat * inner(grad(x), grad(v)) * dx).array()
+    dR_dz = assemble(z_hat * inner(grad(x), grad(v)) * solver.dx).array()
     dR_r_dz = np.dot(psi.T, dR_dz)
     grad_G = dR_dz @ lambda_f + lambda_r @ dR_r_dz
 
     return -grad_G
 
-def cost_functional(z_vec, psi, phi, V):
+def cost_functional(z_vec, psi, phi, solver):
     '''
     Computes the error between the QoI produced by the full vs the 
     reduced order model (0.5 * || y - y_r ||_2^2)
     '''
-    z = Function(V)
+    z = Function(solver.V)
     z.vector().set_local(z_vec)
-    x, y, A, B, C, dA_dz = forward(z, V)
-    A_r, B_r, C_r, x_r, y_r = reduced_forward(A, B, C, psi, phi)
+    x, y, A, B, C = solver.forward(z)
+    A_r, B_r, C_r, x_r, y_r = solver.reduced_forward(A, B, C, psi, phi)
 
     # Since we are using a minimizer, cost function has to be negated
     cost = -0.5 * (y - y_r)**2
@@ -54,26 +51,27 @@ def cost_functional(z_vec, psi, phi, V):
     #  print("\nCost at given basis: {}\n".format(-cost))
     return cost
 
-def optimize(z_0, phi, V):
+def optimize(z_0, phi, solver):
     '''
     Finds the parameter with the maximum ROM error given a starting point z_0
     '''
-    w, y, A, B, C, dA_dz = forward(z_0, V)
+    w, y, A, B, C = solver.forward(z_0)
     psi = np.dot(A, phi)
-    init_cost = -cost_functional(z_0.vector()[:], psi, phi, V)
+    init_cost = -cost_functional(z_0.vector()[:], psi, phi, solver)
 
     z_bounds = scipy.optimize.Bounds(0.1, 10)
-    optimize_result = scipy.optimize.minimize(cost_functional, 
+    res = scipy.optimize.minimize(cost_functional, 
                             z_0.vector()[:], 
-                            args=(psi, phi, V), 
+                            args=(psi, phi, solver), 
                             method='L-BFGS-B', 
                             jac=gradient,
-                            options={'maxiter':20}, #Hilariously low
+                            options={'maxiter':200}, 
                             bounds=z_bounds)
 
-    print("Optimizer return with message: {}".format(optimize_result.message))
-    print("Initial G: {}, Final G: {}".format(init_cost, -optimize_result.fun))
-    z_star = Function(V)
-    z_star.vector().set_local(optimize_result.x)
-    g_z_star = optimize_result.fun
+    print("Optimizer message: {}".format(res.message))
+    print("Cost functional evaluations: {}, Opt. Iterations: {}".format(res.nfev, res.nit))
+    print("Initial G: {}, Final G: {}".format(init_cost, -res.fun))
+    z_star = Function(solver.V)
+    z_star.vector().set_local(res.x)
+    g_z_star = -res.fun
     return z_star, g_z_star

applications/fin.py
@@ -5,9 +5,9 @@ import matplotlib.pyplot as plt
 from dolfin import *
 import mshr
 import numpy as np
-from forward_solve import forward, reduced_forward
+from forward_solve import Fin
 from error_optimization import optimize
-from model_constr_adaptive_sampling import sample
+from model_constr_adaptive_sampling import sample, enrich
 
 set_log_level(40)
 
@@ -26,6 +26,7 @@ mesh = mshr.generate_mesh(geometry, 40)
 
 V = FunctionSpace(mesh, 'CG', 1)
 dofs = len(V.dofmap().dofs())
+solver = Fin(V)
 
 ##########################################################3
 # Basis initialization with dummy solves and POD
@@ -48,7 +49,7 @@ for i in range(0,samples):
         m = Function(V)
         m.vector().set_local(np.random.uniform(0.1, 10.0, dofs))
 
-    w = forward(m, V)[0]
+    w = solver.forward(m)[0]
     Y[i,:] = w.vector()[:]
 
 K = np.dot(Y, Y.T)
@@ -63,24 +64,27 @@ for i in range(basis_size):
     U[i,:] = np.sum(np.dot(np.diag(e_i), Y),0)
 
 basis = U.T
-z_0 = Function(V)
-z_0.vector().set_local(np.random.uniform(0.1, 10, dofs))
 
 t_i = time.time()
-basis = sample(basis, z_0, optimize, forward, V)
+def random_initial():
+    z_0 = Function(V)
+    z_0.vector().set_local(np.random.uniform(0.1, 10.0, dofs))
+    return z_0
+
+basis = sample(basis, random_initial, optimize, solver)
 t_f = time.time()
 print("Sampling time taken: {}".format(t_f - t_i))
 print("Computed basis with shape {}".format(basis.shape))
 
 m = interpolate(Expression("2*x[1] + 1.0", degree=2), V)
-w, y, A, B, C, dA_dz  = forward(m, V)
+w, y, A, B, C  = solver.forward(m)
 p = plot(m, title="Conductivity")
 plt.colorbar(p)
 plt.show()
 p = plot(w, title="Temperature")
 plt.colorbar(p)
 plt.show()
-A_r, B_r, C_r, x_r, y_r = reduced_forward(A, B, C, np.dot(A, basis), basis) 
+A_r, B_r, C_r, x_r, y_r = solver.reduced_forward(A, B, C, np.dot(A, basis), basis) 
 x_tilde = np.dot(basis, x_r)
 x_tilde_f = Function(V)
 x_tilde_f.vector().set_local(x_tilde)
@@ -88,5 +92,22 @@ p = plot(x_tilde_f, title="Temperature reduced")
 plt.colorbar(p)
 plt.show()
 
-print("Reduced system error: {}".format(np.linalg.norm(y-y_r)))
+print("Reduced system relative error: {}".format(np.linalg.norm(y-y_r)/np.abs(y)))
 np.savetxt("basis.txt", basis, delimiter=",")
+
+#Modify basis
+w = w.vector()[:]
+w = w.reshape(w.shape[0], 1)
+basis = enrich(basis, w)
+
+A_r, B_r, C_r, x_r, y_r = solver.reduced_forward(A, B, C, np.dot(A, basis), basis) 
+x_tilde = np.dot(basis, x_r)
+x_tilde_f = Function(V)
+x_tilde_f.vector().set_local(x_tilde)
+p = plot(x_tilde_f, title="Temperature reduced")
+plt.colorbar(p)
+plt.show()
+
+print("Reduced system relative error with added snapshot: {}".format(np.linalg.norm(y-y_r)/np.abs(y)))
+np.savetxt("basis.txt", basis, delimiter=",")
+

applications/fin_eigen_example.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 from dolfin import *
 import mshr
 import numpy as np
-from forward_solve import forward 
+from forward_solve import Fin 
 
 # Create a fin geometry
 geometry = mshr.Rectangle(Point(2.5, 0.0), Point(3.5, 4.0)) \
@@ -23,6 +23,7 @@ plt.show()
 
 V = FunctionSpace(mesh, 'CG', 1)
 dofs = len(V.dofmap().dofs())
+solver = Fin(V)
 
 samples = 5
 Y = np.zeros((samples, dofs))
@@ -33,7 +34,7 @@ for i in range(0,samples):
     else:
         m = interpolate(Expression("c*x[0] + 0.1", degree=2, c=2.0*i), V)
 
-    w = forward(m, V)
+    w = solver.forward(m)[0]
 
     #  if i%45 == 0:
         #  p = plot(w, title="Temperature")

applications/forward_solve.py
@@ -1,100 +1,97 @@
 from dolfin import *
 import numpy as np
 
-def averaging_operator(V):
-    # Kinda hacky way to get C as the averaging operator over the domain. Probably a better way
-    v = TestFunction(V)
-    d_omega_f = interpolate(Expression("1.0", degree=2), V)
-    domain_integral = assemble(v*dx)
-    domain_measure = np.dot(np.array(d_omega_f.vector()[:]), np.array(domain_integral[:]))
-    C = domain_integral/domain_measure
-    C = C[:]
-    return C
+class Fin:
+    def __init__(self, V):
+        self.V = V
+        self.Bi = Constant(0.1)
 
-def get_weak_forms(m, V):
-    mesh = V.mesh()
-    w = TrialFunction(V)
-    v = TestFunction(V)
+        mesh = V.mesh()
+        self.w = TrialFunction(V)
+        self.v = TestFunction(V)
 
-    domains = MeshFunction("size_t", mesh, mesh.topology().dim())
-    domains.set_all(0)
+        domains = MeshFunction("size_t", mesh, mesh.topology().dim())
+        domains.set_all(0)
 
-    # boundary conditions
-    bottom = CompiledSubDomain("near(x[1], side) && on_boundary", side = 0.0)
-    exterior = CompiledSubDomain("!near(x[1], side) && on_boundary", side = 0.0)
-    boundaries = MeshFunction("size_t", mesh, mesh.topology().dim()-1)
-    boundaries.set_all(0)
-    exterior.mark(boundaries, 1)
-    bottom.mark(boundaries, 2)
+        # boundary conditions
+        bottom = CompiledSubDomain("near(x[1], side) && on_boundary", side = 0.0)
+        exterior = CompiledSubDomain("!near(x[1], side) && on_boundary", side = 0.0)
+        boundaries = MeshFunction("size_t", mesh, mesh.topology().dim()-1)
+        boundaries.set_all(0)
+        exterior.mark(boundaries, 1)
+        bottom.mark(boundaries, 2)
 
-    dx = Measure('dx', domain=mesh, subdomain_data=domains)
-    ds = Measure('ds', domain=mesh, subdomain_data=boundaries)
+        self.dx = Measure('dx', domain=mesh, subdomain_data=domains)
+        self.ds = Measure('ds', domain=mesh, subdomain_data=boundaries)
+        self.a = self.v * self.dx
+        self.B = assemble(self.a)[:]
+        self.C, self.domain_measure = self.averaging_operator()
 
-    # Setting up the variational form
-    Bi = Constant(0.1)
-    F = inner(m * grad(w), grad(v)) * dx(0) + v * Bi * w * ds(1)
-    a = v * ds(2)
+    def averaging_operator(self):
+        # Kinda hacky way to get C as the averaging operator over the domain. Probably a better way
+        v = TestFunction(self.V)
+        d_omega_f = interpolate(Expression("1.0", degree=2), self.V)
+        domain_integral = assemble(v * self.dx)
+        domain_measure = assemble(d_omega_f * self.dx)
+        C = domain_integral/domain_measure
+        C = C[:]
+        return C, domain_measure
 
-    dF_dm = inner(grad(w), grad(v)) * dx(0)
-    return F, a, dF_dm
+    def get_weak_forms(self, m):
+        # Setting up the variational form
+        F = inner(m * grad(self.w), grad(self.v)) * self.dx(0) + self.v * self.Bi * self.w * self.ds(1)
+        return F, self.a
 
-def forward(m, V):
-    '''
-    Performs a forward solve to obtain temperature distribution
-    given the conductivity field m and FunctionSpace V.
-    This solve assumes Biot number to be a constant.
-    Returns:
-     w - Temperature field 
-     y - Average temperature
-     A - Mass matrix
-     B - Discretized RHS
-     C - Averaging operator
-     dA_dz - Partial derivative of the mass matrix w.r.t. the parameters
-    '''
+    def forward(self, m):
+        '''
+        Performs a forward solve to obtain temperature distribution
+        given the conductivity field m and FunctionSpace V.
+        This solve assumes Biot number to be a constant.
+        Returns:
+         w - Temperature field 
+         y - Average temperature
+         A - Mass matrix
+         B - Discretized RHS
+         C - Averaging operator
+         dA_dz - Partial derivative of the mass matrix w.r.t. the parameters
+        '''
 
-    F, a, dF_dm = get_weak_forms(m, V)
-    A = assemble(F).array()
-    B = assemble(a)[:]
+        F, a = self.get_weak_forms(m)
+        A = assemble(F).array()
 
-    #TODO: This has to be a tensor so this is incorrect
-    dA_dz = assemble(dF_dm).array()
+        z = Function(self.V)
+        solve(F == self.a, z) 
 
-    w = Function(V)
-    solve(F == a, w) 
+        # TODO: Compute quantity of interest
+        #  w_nodal_values = np.array(w.vector()[:]) 
+        #  y = np.mean(w_nodal_values)
+        y = assemble(z * self.dx)/self.domain_measure
+        #  print("Average temperature: {}".format(y))
 
-    # TODO: Compute quantity of interest
-    w_nodal_values = np.array(w.vector()[:]) 
-    y = np.mean(w_nodal_values)
-    #  print("Average temperature: {}".format(y))
+        return z, y, A, self.B, self.C
 
-    C = averaging_operator(V)
+    def reduced_forward(self, A, B, C, psi, phi):
+        '''
+        Returns the reduced matrices given a reduced trial and test basis
+        to solve reduced system of A(z)x = B(z)
+        Arguments:
+        A   - LHS forward operator in A(z)x = B(z)
+        B   - RHS in A(z)x = B(z)
+        psi - Trial basis
+        phi - Test basis
+        '''
+        A_r = np.dot(psi.T, np.dot(A, phi))
+        B_r = np.dot(psi.T, B)
+        C_r = np.dot(C, phi)
 
-    return w, y, A, B, C, dA_dz
+        #Solve reduced system to obtain x_r
+        x_r = np.linalg.solve(A_r, B_r)
+        y_r = np.dot(C_r, x_r)
 
-def reduced_forward(A, B, C, psi, phi):
-    '''
-    Returns the reduced matrices given a reduced trial and test basis
-    to solve reduced system of A(z)x = B(z)
-    Arguments:
-    A   - LHS forward operator in A(z)x = B(z)
-    B   - RHS in A(z)x = B(z)
-    psi - Trial basis
-    phi - Test basis
-    '''
-    A_r = np.dot(psi.T, np.dot(A, phi))
-    B_r = np.dot(psi.T, B)
-    C_r = np.dot(C, phi)
+        return A_r, B_r, C_r, x_r, y_r
 
-    #Solve reduced system to obtain x_r
-    x_r = np.linalg.solve(A_r, B_r)
-    y_r = np.dot(C_r, x_r)
+    def reduced_forward_no_full(self, m, psi, phi):
+        F, a = self.get_weak_forms(m)
+        A = assemble(F).array()
 
-    return A_r, B_r, C_r, x_r, y_r
-
-def reduced_forward_no_full(m, V, psi, phi):
-    F, a, dF_dm = get_weak_forms(m, V)
-    A = assemble(F).array()
-    B = assemble(a)[:]
-    C = averaging_operator(V)
-
-    return reduced_forward(A,B,C, psi, phi)
+        return reduced_forward(A, self.B,self.C, psi, phi)

applications/rom_err_predict.py
@@ -7,7 +7,7 @@ import tensorflow as tf
 from pandas import DataFrame, read_csv
 import pdb
 from numpy.random import rand
-from forward_solve import *
+from forward_solve import Fin
 from fin_functionspace import get_space
 
 class FinInput:
@@ -17,6 +17,7 @@ class FinInput:
         self.dofs = len(self.V.dofmap().dofs())
         self.phi = np.loadtxt('basis.txt',delimiter=",")
         self.batch_size = batch_size
+        self.solver = Fin(self.V)
 
     def train_input_fn(self):
         params = np.random.uniform(0.1, 1, (self.batch_size, self.dofs))
@@ -25,9 +26,9 @@ class FinInput:
         for i in range(self.batch_size):
             m = Function(self.V)
             m.vector().set_local(params[i,:])
-            w, y, A, B, C, dA_dz = forward(m, self.V)
+            w, y, A, B, C = solver.forward(m)
             psi = np.dot(A, self.phi)
-            A_r, B_r, C_r, x_r, y_r = reduced_forward(A, B, C, psi, self.phi)
+            A_r, B_r, C_r, x_r, y_r = solver.reduced_forward(A, B, C, psi, self.phi)
             errors[i][0] = y - y_r 
 
         return ({'x':tf.convert_to_tensor(params)}, tf.convert_to_tensor(errors))
@@ -39,9 +40,9 @@ class FinInput:
         for i in range(self.batch_size):
             m = Function(self.V)
             m.vector().set_local(params[i,:])
-            w, y, A, B, C, dA_dz = forward(m, self.V)
+            w, y, A, B, C = solver.forward(m)
             psi = np.dot(A, self.phi)
-            A_r, B_r, C_r, x_r, y_r = reduced_forward(A, B, C, psi, self.phi)
+            A_r, B_r, C_r, x_r, y_r = solver.reduced_forward(A, B, C, psi, self.phi)
             errors[i][0] = y - y_r 
 
         return ({'x':tf.convert_to_tensor(params)}, tf.convert_to_tensor(errors))
@@ -71,9 +72,9 @@ def main(argv):
     print(eval_result)
 
     m = interpolate(Expression("2*x[0] + 3*x[1] + 1.5", degree=2), finInstance.V)
-    w, y, A, B, C, dA_dz = forward(m, finInstance.V)
+    w, y, A, B, C = solver.forward(m)
     psi = np.dot(A, finInstance.phi)
-    A_r, B_r, C_r, x_r, y_r = reduced_forward(A, B, C, psi, finInstance.phi)
+    A_r, B_r, C_r, x_r, y_r = solver.reduced_forward(A, B, C, psi, finInstance.phi)
     error = y - y_r
 
     #  pred_input_fn = tf.estimator.inputs.numpy_input_fn(