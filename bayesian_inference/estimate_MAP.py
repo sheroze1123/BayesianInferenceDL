3753b34f4717a38c95f7aca8ea04054df44f2d74
Sheroze Sheriffdeen <sheroze.sheriffdeen@utexas.edu>
2019-10-15 10:32:29 -0500

    added gradient check to MAP estimate cost function

--- bayesian_inference/estimate_MAP.py
+++ bayesian_inference/estimate_MAP.py
@@ -58,19 +58,55 @@ class SolverWrapper:
         self.cost = None
         self.grad = None
         self.z = dl.Function(V)
+        self.z_t = dl.TestFunction(V)
+        self.gamma = dl.Constant(0.001)
+        self.grad_reg = self.gamma * dl.inner(dl.grad(self.z), dl.grad(self.z_t)) * dl.dx
+        self.reg = 0.5 * self.gamma * dl.inner(dl.grad(self.z), dl.grad(self.z)) * dl.dx
 
     def cost_function(self, z_v):
         self.z.vector().set_local(z_v)
         self.grad, self.cost = self.solver_r.grad_romml(self.z)
-        return self.cost
+        cost  = self.cost + dl.assemble(self.reg)
+        return cost
 
     def gradient(self, z_v):
         self.z.vector().set_local(z_v)
         self.grad, self.cost = self.solver_r.grad_romml(self.z)
-        return self.grad
+        grad = self.grad + self.grad_reg_eval(z_v)
+        return grad
+
+    def grad_reg_eval(self, z_v):
+        self.z.vector().set_local(z_v)
+        return dl.assemble(self.grad_reg)[:]
 
 solver_w = SolverWrapper(err_model, solver_r)
-bounds = Bounds(0.1, 5)
+
+norm = np.random.randn(len(chol))
+eps_z = np.exp(0.5 * chol.T @ norm)
+z.vector().set_local(eps_z)
+eps_norm = np.sqrt(dl.assemble(dl.inner(z,z)*dl.dx))
+eps_z = eps_z/eps_norm
+norm = np.random.randn(len(chol))
+z_ = np.exp(0.5 * chol.T @ norm)
+n_eps = 16
+hs = np.power(2., -np.arange(n_eps))
+dir_grad = np.dot(solver_w.gradient(z_), eps_z)
+err_grads = []
+for h in hs:
+    a_g = (solver_w.cost_function(z_) - solver_w.cost_function(z_ + h * eps_z))/h
+    err = abs(a_g - dir_grad)
+    err_grads.append(err)
+
+plt.loglog(hs, err_grads, "-ob", label="Error Grad")
+plt.loglog(hs, (.5*err_grads[0]/hs[0])*hs, "-.k", label="First Order")
+print(f"Directional gradient: {dir_grad}")
+print(f"Errors in approx gradients: {err_grads}")
+plt.savefig('grad_test.png', dpi=200)
+plt.cla()
+plt.clf()
+
+
+bounds = Bounds(0.1, 4)
 res = minimize(solver_w.cost_function, z_0_nodal_vals, 
         method='L-BFGS-B', 
         jac=solver_w.gradient,
@@ -83,7 +119,9 @@ z.vector().set_local(res.x)
 
 w,y, _, _, _ = solver.forward(z)
 pred_obs = solver.qoi_operator(w)
+obs_err = np.linalg.norm(pred_obs - obs_data)
 print(f"True: {obs_data}\n Pred: {pred_obs}")
+print(f"Observation error: {obs_err}")
 
 p = dl.plot(z)
 plt.colorbar(p)
@@ -94,3 +132,9 @@ plt.clf()
 p = dl.plot(z_true)
 plt.colorbar(p)
 plt.savefig("z_true.png")
+
+reconst_err = dl.assemble(dl.inner(z - z_true, z - z_true) * dl.dx)
+z_true_norm = dl.assemble(dl.inner(z_true, z_true) * dl.dx)
+rel_r_err = reconst_err/z_true_norm
+print(f"Relative reconstruction error: {rel_r_err * 100} %")
+print(f"Reconstruction error: {reconst_err}")
